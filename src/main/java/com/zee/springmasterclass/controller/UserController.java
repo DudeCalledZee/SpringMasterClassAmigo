fec07d73b784fc41b1236c2c51b5621dba8f3386
Zee <ineedtothink@gmail.com>
2020-05-30 20:03:19 +0100

    adds ability to add,remove and update users

--- src/main/java/com/zee/springmasterclass/controller/UserController.java
+++ src/main/java/com/zee/springmasterclass/controller/UserController.java
@@ -7,9 +7,14 @@ import java.util.Optional;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +41,33 @@ public class UserController {
       return ResponseEntity.ok(userOptional.get());
     }
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessage("User with " + uuId + " Id not found"));
+  }
+
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Integer> addUser(@RequestBody User user){
+    int addUserResult = userService.addUser(user);
+    if (addUserResult == 1){
+      return ResponseEntity.ok().build();
+    }
+    return ResponseEntity.badRequest().build();
+  }
+
+  @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Integer> updateUser(@RequestBody User user) {
+    int updateUserResult = userService.updateUser(user);
+    if (updateUserResult == 1){
+      return ResponseEntity.ok().build();
+    }
+    return ResponseEntity.badRequest().build();
+  }
+
+  @DeleteMapping(path = "{userId}")
+  public ResponseEntity<Integer> deleteUser(@PathVariable("userId") UUID uuId){
+    int deleteUserResult = userService.removeUser(uuId);
+    if (deleteUserResult == 1){
+      return ResponseEntity.ok().build();
+    }
+    return ResponseEntity.badRequest().build();
   }
 
   class ErrorMessage {
